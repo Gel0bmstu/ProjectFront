56d326a6685a34f56509115d7abac3703ec9dbf2
Gel0bmstu <Leshikne@gmail.com>
2019-03-02 22:16:00 +0300

    Из говна и палок

--- public/main.js
+++ public/main.js
@@ -8,64 +8,158 @@ const menuItems = {
 	about: 'О приложении'
 };
 
-const app = document.getElementById('application');
-const block = document.createElement('div');
-block.className = 'menuBlock';
-app.appendChild(block);
+// const head = document.getElementsByTagName('head');
+// const body = document.getElementsByTagName('body');
 
-function createTitle(object) {
-	const title = document.createElement('h1');
-	title.textContent = 'ВСЕМ МОИМ БРАТЬЯМ САЛАААААААААААААААААМ';
+// Разименовывем необходимые элементы DOM'a
+const app = document.getElementById("application");
+
+const header = document.createElement('div');
+header.dataset.section = "header";
+app.appendChild(header);
+
+const side = document.createElement('div');
+side.dataset.section = "sidebar";
+app.appendChild(side);
+
+const main = document.createElement('div');
+main.className = "main";
+app.appendChild(main);
+
+// Вспомогательные функции (их и будем писать/исправлять/дополнять)
+function createTitle(text) {
+	const title = document.createElement('div');
 	title.className = 'title';
+	const title_txt = document.createElement('h1');
+	title_txt.textContent = text;
 
-	object.appendChild(title);
+	main.appendChild(title)
+	title.appendChild(title_txt);
 }
 
-function createButton(object, text) {
+function createButton(object, sectionName, className) {
 	const btn = document.createElement('button');
-	btn.dataset.section = text;
-	btn.textContent = text;
+	btn.dataset.section = sectionName;
+	btn.textContent = sectionName;
+	btn.className = className;
 	object.append(btn);
 }
 
+function createForm(object, sectionName, className, formAction) {
+	const form = document.createElement('input');
+	form.dataset.section = sectionName;
+	form.placeholder = formAction;
+	form.className = className;
+	form.action = formAction;
+	object.append(form);
+}
+
+// Основные функции, отвечают за логику работы фронта/генерирование контента и тд
 function createMenu() {
-	block.innerHTML = '';
-	createTitle(block);	
+	main.innerHTML = '';
+	createTitle('ВСЕМ МОИМ БРАТЬЯМ САЛАААААААААААААААААМ');
+
+	const menu = document.createElement('div');
+	menu.className = 'menu';
+	main.appendChild(menu);
+
+	const menuStyle = document.createElement('link');
+	menuStyle.rel = 'stylesheet'
+	menuStyle.href = 'styles/menu.css';
+
+	// poka hz kak eto
+	header.appendChild(menuStyle);
+
 	Object.keys(menuItems).forEach( (key) => {
-		createButton(block, key);
+		createButton(menu, key, 'btn');
 	});
 }	
 
 function createLogin() {
-	block.innerHTML = '';
-	createButton(block, 'manipulation');
-	createButton(block, 'back')
+	main.innerHTML = '';
+
+	createTitle('Login')
+
+	const menu = document.createElement('div');
+	menu.className = 'menu';
+	main.appendChild(menu);
+
+	createForm(menu, 'loginForm', 'loginForm form', 'login');
+	createForm(menu, 'passForm', 'passForm form', 'pass');
+	createButton(menu, 'submit', 'submit btn');
+	createButton(menu, 'menu', 'menu btn');
+
+
 }
 
-function back() {
-	createMenu();
+function createSignup() {
+	
 }
 
+function createGame() {
+	main.innerHTML = '';
+
+	const menu = document.createElement('div');
+	menu.className = 'gameBlock';
+	main.appendChild(menu);
+
+	const canvas = document.createElement('canvas');
+	canvas.id = 'gameCanvas';
+	canvas.width = 700;
+	canvas.height = 700;
+	menu.appendChild(canvas);
+
+	const buf = document.getElementById('gamejs');
+	if (buf !== null) {
+		alert('got!');
+	}
+
+	const gameLogic = document.createElement('script');
+	gameLogic.id = "gamejs";
+	gameLogic.src = '/game.js'
+	menu.appendChild(gameLogic);
+
+	createButton(menu, 'menu', 'btn');
+}
+
+// Заглушки, можешь сделать их, если хочешь
+function leaderboard() {
+
+}
+
+function createAbout() {
+
+}
+
+
+// Главный блок: создаем меню 
 createMenu();
 
-
+// Карта функций, сюда будем подгруать новые функции
 const functions = {
-	menu: createMenu, 
-	title: createTitle,
-	// signup: createSignup,
+	menu: createMenu,
+
+	// Menu elements
+	signup: createSignup,
 	login: createLogin,
-	back: back
-	// game: createGame,
+	game: createGame,
 	// leaderboard: createLeaderboard,
-	// about: createAbout
+	// about: createAbout,
+
+	// Other functions
+	title: createTitle,
 };
 
+// Обработчик всех евентов в DOM'е
 app.addEventListener('click', (evt) => {
-	evt.preventDefault();
 
 	const target = evt.target;
 
-	const section = target.dataset.section;
+	if (target instanceof HTMLButtonElement) {	
+		evt.preventDefault();
 
-	functions[section]();
+		const section = target.dataset.section;
+
+		functions[section]();
+	};
 });
